0d3e5e082c35b691d95f5f30c636dec5444c14cb
Constantine Molchanov <moigagoo@duck.com>
2026-01-31 01:08:12 +0400

    Make Dockerfiles compliant with the official images guidelines.

--- src/dhbp/flavors/slim.nim
+++ src/dhbp/flavors/slim.nim
@@ -1,32 +1,76 @@
 #? stdtmpl
-#proc ubuntu*(version: string,
-#             labels: openarray[(string, string)] = {:}): string =
-FROM ubuntu:latest
+#proc ubuntu*(version: string, labels: openarray[(string, string)] = {:}): string =
+#  result = ""
+FROM ubuntu:noble
+
+ENV NIM_VERSION=$version
+ENV PATH="/usr/local/bin:$${PATH}"
+
 #  for label, value in labels.items:
 LABEL $label="$value"
 #  end for
-RUN apt-get update; apt-get install -y wget xz-utils g++; \
-    wget -qO- https://deb.nodesource.com/setup_13.x | bash -; \
-    apt-get install -y nodejs
-RUN wget https://nim-lang.org/download/nim-${version}.tar.xz; \
-    tar xf nim-${version}.tar.xz; rm nim-${version}.tar.xz; \
-    mv nim-${version} nim; \
-    cd nim; sh build.sh; \
-    rm -r c_code tests; \
-    ln -s `pwd`/bin/nim /bin/nim
+
+RUN set -eux; \
+    apt-get update; \
+    apt-get install -y --no-install-recommends \
+        ca-certificates \
+        g++ \
+        libssl-dev \
+        wget \
+        xz-utils \
+    ; \
+    \
+    wget -O nim.tar.xz "https://nim-lang.org/download/nim-${NIM_VERSION}.tar.xz"; \
+    mkdir -p /usr/local/src/nim; \
+    tar -xf nim.tar.xz -C /usr/local/src/nim --strip-components=1; \
+    rm nim.tar.xz; \
+    \
+    cd /usr/local/src/nim; \
+    sh build.sh; \
+    \
+    ln -s /usr/local/src/nim/bin/nim /usr/local/bin/nim; \
+    \
+    rm -rf /usr/local/src/nim/c_code /usr/local/src/nim/tests; \
+    apt-get purge -y --auto-remove -o APT::AutoRemove::RecommendsImportant=false; \
+    rm -rf /var/lib/apt/lists/*
+
+WORKDIR /app
+CMD ["nim", "--version"]
 #end proc
 #
-#proc alpine*(version: string,
-#             labels: openarray[(string, string)] = {:}): string =
+#proc alpine*(version: string, labels: openarray[(string, string)] = {:}): string =
+#  result = ""
 FROM alpine:3.20
+
+ENV NIM_VERSION=$version
+ENV PATH="/usr/local/bin:$${PATH}"
+
 #  for label, value in labels.items:
 LABEL $label="$value"
 #  end for
-RUN apk add --no-cache g++ curl tar xz nodejs
-RUN mkdir -p /nim; \
-    curl -sL "https://nim-lang.org/download/nim-${version}.tar.xz" \
-    |tar xJ --strip-components=1 -C /nim; \
-    cd /nim; sh build.sh; \
-    rm -r c_code tests; \
-    ln -s `pwd`/bin/nim /bin/nim
+
+RUN set -eux; \
+    apk add --no-cache \
+        ca-certificates \
+        g++ \
+        libgcc \
+        openssl-dev \
+        wget \
+        xz \
+    ; \
+    \
+    wget -O nim.tar.xz "https://nim-lang.org/download/nim-${NIM_VERSION}.tar.xz"; \
+    mkdir -p /usr/local/src/nim; \
+    tar -xf nim.tar.xz -C /usr/local/src/nim --strip-components=1; \
+    rm nim.tar.xz; \
+    \
+    cd /usr/local/src/nim; \
+    sh build.sh; \
+    \
+    ln -s /usr/local/src/nim/bin/nim /usr/local/bin/nim; \
+    \
+    rm -rf /usr/local/src/nim/c_code /usr/local/src/nim/tests
+
+WORKDIR /app
+CMD ["nim", "--version"]
 #end proc
